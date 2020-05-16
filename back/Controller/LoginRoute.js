ee76833bde15f26dcf067ac2d82fda2c5d50fd4a
hyhae <helsherif95@gmail.com>
2020-05-17 01:08:06 +0200

    Updated connection pool, and token expires now in 40 seconds

--- back/Controller/LoginRoute.js
+++ back/Controller/LoginRoute.js
@@ -7,7 +7,7 @@ const loginServices = require("../Services/LoginServices")
 router.post("", (req, res) => {
         loginServices.authenticateAndReturnUser(req.body.username, req.body.password)
         .then((result) => {
-            const accessToken = jwt.sign({UID:result[0].UID, username:result[0].username, role:result[0].role}, process.env.ACCESS_TOKEN_SECRET)
+            const accessToken = jwt.sign({UID:result[0].UID, username:result[0].username, role:result[0].role}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '40s'})
             res.status(201).json({accessToken:accessToken})
             res.end()
         })
--- back/Repository/ConnectionHandler.js
+++ back/Repository/ConnectionHandler.js
@@ -1,22 +1,13 @@
 const mysql = require("mysql")
 
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = mysql.createPool({
+    connectionLimit: process.env.DB_CONN_LIMIT,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_SCHEMA,
     multipleStatements: true
-});
-
-mysqlConnection.connect((err)=>{
-    if(!err){
-        console.log("Connection succeeded");
-    }
-    else{
-        console.log(err.stack);
-        console.log("Connection failed");
-    }
 });
 
 module.exports = mysqlConnection;
